525c5bc79fae330c99cddfaabbb798c996e08884
geosearchef <geosearchef@googlemail.com>
2020-11-18 18:50:06 +0100

    pi commutation rule

---
+++ src/main/kotlin/zxn/calculus/PiCommutationRule.kt
@@ -0,0 +1,54 @@
+package zxn.calculus
+
+import util.math.equalsNumerically
+import zxn.network.Spider
+import zxn.network.ZXNetwork
+import zxn.network.ZXNode
+import kotlin.math.PI
+
+object PiCommutationRule : ZXRule("PiComm", "K2", false) {
+    override val inverse = PiCommutationRuleInverse
+
+    override fun apply(selectedNodes: List<ZXNode>, network: ZXNetwork, dryRun: Boolean): Boolean {
+        if(! verifySpiderCount(selectedNodes, 2)) {
+            return false
+        }
+
+        val piSpider = selectedNodes.map { it as Spider }.find { it.phase.equalsNumerically(PI, 0.01)} ?: return false
+        val alphaSpider = selectedNodes.find { it != piSpider } as Spider? ?: return false
+        val piNeighborhood = network.getNeighborhood(piSpider)
+        val alphaNeighborhood = network.getNeighborhood(alphaSpider)
+
+        if(piSpider.color == alphaSpider.color) {
+            return false
+        }
+
+        if(piNeighborhood[alphaSpider] != 1 || alphaNeighborhood[piSpider] != 1 || piNeighborhood.values.sum() != 2 || alphaNeighborhood.values.sum() != 2) {
+            return false
+        }
+
+        val externalNodePi = piNeighborhood.keys.find { it != alphaSpider } ?: return false
+        val externalNodeAlpha = piNeighborhood.keys.find { it != piSpider } ?: return false
+
+        if(dryRun) {
+            return true
+        }
+
+        // APPLY
+
+        piSpider.toggleColor()
+        alphaSpider.toggleColor()
+
+        piSpider.phase = alphaSpider.phase
+        alphaSpider.phase = PI
+
+        return true
+    }
+}
+
+object PiCommutationRuleInverse : ZXRule("PiComm Inv", "K2_I", true) {
+    override val inverse = PiCommutationRule
+
+    override fun apply(selectedNodes: List<ZXNode>, network: ZXNetwork, dryRun: Boolean): Boolean = inverse.apply(selectedNodes, network, dryRun)
+
+}
--- src/main/kotlin/zxn/calculus/ZXRule.kt
+++ src/main/kotlin/zxn/calculus/ZXRule.kt
@@ -2,16 +2,18 @@ package zxn.calculus
 
 import util.math.equalsNumerically
 import zxn.network.*
+import kotlin.math.PI
 
 abstract class ZXRule(val longName: String, val shortName:String, val isInverse: Boolean) {
 
     companion object {
         val allRules = listOf(
-            SpiderRule1,
-            SpiderRule2,
-            CopyingRuleB1,
-            HopfLawH,
-            BialgebraLawB2
+                SpiderRule1,
+                SpiderRule2,
+                CopyingRuleB1,
+                HopfLawH,
+                BialgebraLawB2,
+                PiCommutationRule
         )
     }
 
@@ -30,6 +32,7 @@ abstract class ZXRule(val longName: String, val shortName:String, val isInverse:
     protected fun verifyCount(selectedNodes: List<ZXNode>, count: Int) = selectedNodes.size == count
     protected fun verifySpiderCount(selectedNodes: List<ZXNode>, count: Int) = verifyCount(selectedNodes, count) && selectedNodes.all { it is Spider }
     protected fun verifySpiderZeroPhaseCount(selectedNodes: List<ZXNode>, count: Int) = verifyCount(selectedNodes, count) && selectedNodes.all { (it as? Spider)?.phase?.equalsNumerically(0.0, 0.001) ?: false }
+    protected fun verifySpiderPiPhaseCount(selectedNodes: List<ZXNode>, count: Int) = verifyCount(selectedNodes, count) && selectedNodes.all { (it as? Spider)?.phase?.equalsNumerically(PI, 0.001) ?: false }
 
     fun List<ZXNode>.spiders() = this.filterIsInstance<Spider>()
     fun List<ZXNode>.spiders(color: Spider.SpiderColor) = spiders().filter { it.color == color }
